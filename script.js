c15151eb93e7814992a2dbb28f1c8c3d6d81f44a
guapoMSI <115550172+guapoMSI@users.noreply.github.com>
2022-12-11 20:11:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let name="Jose"
+console.log(name);
+
+let age="21"
+console.log(age);
+
+console.log(age*7)
+let age_multiplicada = age * 7;
+
+let person = {
+    name: "José",
+    age: 21,
+    isStudent: true
+  };
+  console.log(person);
+
+  let amigos = ["David", "André", "Leticia"];
+let numbers = [1, 2, 3];
+console.log(amigos[0]);
+    
+
+let friends= [
+    {
+        name: "David",
+        age: 18,
+        isStudent: true
+    },
+    {
+        name: "André",
+        age: 18,
+        isStudent: true
+    },
+    {
+        name: "Leticia",
+        age: 19,
+        isStudent: true}
+];
+console.log(friends[0]);
+
+
+
+
